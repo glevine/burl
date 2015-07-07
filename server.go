172e52dcd2529d1c254fe12997ecda1529df9d30
Greg Levine <levine.greg@gmail.com>
2015-07-06 21:16:09 -0400

    Added a subrouter for /urls/ to serve saved resources

--- server.go
+++ server.go
@@ -8,12 +8,21 @@ import (
 )
 
 func main() {
-    router := mux.NewRouter()
-    router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-      fmt.Fprintf(w, "Welcome to the home page!")
-    })
+    router := mux.NewRouter().StrictSlash(true)
+    router.Path("/").HandlerFunc(HomeHandler).Name("home")
+
+    resources := router.PathPrefix("/urls").Subrouter()
+    resources.Methods("GET").Path("/").HandlerFunc(ResourcesIndexHandler).Name("resources_index")
 
     n := negroni.Classic()
     n.UseHandler(router)
     n.Run(":8080")
+}
+
+func HomeHandler(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(w, "Welcome to the home page!")
+}
+
+func ResourcesIndexHandler(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(w, "Get me some urls!")
 }
